51e4a625f6d2367b6e6f19ba102de4e466ea2713
jagDemon <yilmazatarik1@gmail.com>
2026-06-20 20:36:18 +0200

    Improve plotting of mergesort result

--- mergesort.py
+++ mergesort.py
@@ -41,11 +41,27 @@ def mergeSort(list_to_sort_by_merge):
 
 import matplotlib.pyplot as plt
 
-my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
+original_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+sorted_list = original_list.copy()
+
+mergeSort(sorted_list)
+
+positions = range(len(original_list))
+
+fig, axes = plt.subplots(1, 2, sharey=True, figsize=(10, 4))
+
+axes[0].bar(positions, original_list)
+axes[0].set_title("Before sorting")
+axes[0].set_xlabel("Index")
+axes[0].set_ylabel("Value")
+axes[0].set_xticks(positions)
+
+axes[1].bar(positions, sorted_list)
+axes[1].set_title("After sorting")
+axes[1].set_xlabel("Index")
+axes[1].set_xticks(positions)
+
+fig.suptitle("Merge Sort: List before and after sorting")
+fig.tight_layout()
+
 plt.show()
